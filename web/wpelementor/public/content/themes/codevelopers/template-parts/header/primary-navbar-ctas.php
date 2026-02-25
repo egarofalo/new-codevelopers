84a21c419035f7004c4da5d7d1ad86b2561d95d6
Edgardo Garofalo <egarofalo83@gmail.com>
2026-02-25 00:47:41 -0300

    task: fix success message styles on quote form

--- web/wpelementor/public/content/themes/codevelopers/template-parts/header/primary-navbar-ctas.php
+++ web/wpelementor/public/content/themes/codevelopers/template-parts/header/primary-navbar-ctas.php
@@ -1,5 +1,5 @@
 <div class="primary-navbar-ctas">
-    <a href="#request-quote-modal" data-toggle="modal" role="button" class="btn btn-primary cta">
+    <a href="<?php echo home_url() ?>#quote-form-section" data-toggle="modal" role="button" class="btn btn-primary cta">
         Pedí tu presupuesto
     </a>
 </div>
